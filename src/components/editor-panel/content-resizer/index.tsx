9540848ecf1820c3ded13963f5faee6473c61ef2
T-N9 <thethoriumtension@gmail.com>
2024-07-09 19:38:00 +0700

    [Icons] Colors updated

--- src/components/editor-panel/content-resizer/index.tsx
+++ src/components/editor-panel/content-resizer/index.tsx
@@ -16,7 +16,7 @@ const ContentResizer = () => {
 
     useEffect(() => {
         setCurrentSize(localizedEditorState?.contentSize ?? 0)
-    },[localizedEditorState?.contentSize])
+    }, [localizedEditorState?.contentSize])
 
     return (
         <>
@@ -26,8 +26,8 @@ const ContentResizer = () => {
             <DropDown
                 disabled={false}
                 buttonClassName="toolbar-item block-controls"
-                buttonIconClassName={''}
-                buttonLabel={'W '+ contentSizers[currentSize]}
+                buttonIconClassName={'icon icon-cube'}
+                buttonLabel={contentSizers[currentSize]}
                 buttonAriaLabel="Width options for content container">
                 {
                     contentSizers.map((size, index) => (
@@ -45,7 +45,10 @@ const ContentResizer = () => {
                             }}
                             className={'item'}
                         >
-                            W{" "}
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#545454" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m21 7.5-2.25-1.313M21 7.5v2.25m0-2.25-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3 2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75 2.25-1.313M12 21.75V19.5m0 2.25-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25" />
+                            </svg>
+
                             {size}
                         </DropDownItem>
                     ))
--- src/components/editor-panel/table-of-content/index.tsx
+++ src/components/editor-panel/table-of-content/index.tsx
@@ -11,12 +11,12 @@ const TableOfContent = () => {
             <button className='inline-block p-1 rounded-md bg-gray-200 shadow' onClick={() => setIsContentShown(!isContentShown)}>
                 {
                     isContentShown ?
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-gray-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#545454" className="size-6 text-gray-500">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5" />
                         </svg>
 
                         :
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-gray-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#545454" className="size-6 text-gray-500">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
                         </svg>
                 }

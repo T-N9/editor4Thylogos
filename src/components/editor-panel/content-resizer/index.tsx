bb262069c34b7dad1311fe0288b61aa630da3d7f
T-N9 <thethoriumtension@gmail.com>
2024-07-09 20:43:41 +0700

    [ContentResizer] size properties updated

--- src/components/editor-panel/content-resizer/index.tsx
+++ src/components/editor-panel/content-resizer/index.tsx
@@ -5,7 +5,7 @@ import { useLocalStorage } from 'react-use'
 import { LocalEditorState } from '../text-editor/TextEditor'
 import { initialData, useEditorState } from '@/context/EditorStateContext'
 
-export const contentSizers = ['845', '1095', '1440']
+export const contentSizers = [{ size: '845', text: 'Small' }, { size: '1095', text: 'Medium' }, { size: '1440', text: 'Large' }]
 
 const ContentResizer = () => {
     const [currentSize, setCurrentSize] = useState<number>(0)
@@ -27,8 +27,8 @@ const ContentResizer = () => {
                 disabled={false}
                 buttonClassName="toolbar-item block-controls"
                 buttonIconClassName={'icon icon-cube'}
-                buttonLabel={contentSizers[currentSize]}
-                buttonAriaLabel="Width options for content container">
+                buttonLabel={contentSizers[currentSize].text}
+                buttonAriaLabel="Select Content Width">
                 {
                     contentSizers.map((size, index) => (
                         <DropDownItem
@@ -49,7 +49,7 @@ const ContentResizer = () => {
                                 <path strokeLinecap="round" strokeLinejoin="round" d="m21 7.5-2.25-1.313M21 7.5v2.25m0-2.25-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3 2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75 2.25-1.313M12 21.75V19.5m0 2.25-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25" />
                             </svg>
 
-                            {size}
+                            {size.text}
                         </DropDownItem>
                     ))
                 }

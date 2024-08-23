ea656ddea30f8b37c447ea0010d5eb7029aa1ea2
T-N9 <thethoriumtension@gmail.com>
2024-08-23 20:28:58 +0700

    [LandingPage] design updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,33 @@
 
 import BlogCatalogue from "@/components/blog-catalogue";
+import { Button } from "@nextui-org/react";
 
 export default function Home() {
   return (
     <section className='text-black'>
       <div className='max-w-[1350px] mx-auto my-5 mb-10'>
-        <h1 className='text-2xl font-bold text-blue-500 mb-3'>TN Notes</h1>
-        <BlogCatalogue />
+        <section className="bg-gray-100 rounded-lg dark:bg-gray-900 text-gray-800 dark:text-gray-100 py-20 px-5 md:px-10">
+          <div className="container mx-auto text-center">
+            <h1 className="text-4xl md:text-6xl font-bold mb-4">
+              Welcome to <span className="text-indigo-600">TN Notes</span>
+            </h1>
+            <p className="text-lg md:text-xl mb-8">
+              A tiny space of <a className="text-indigo-600 underline" href="https://www.tenyain.com/" target="_blank" rel="noreferrer">mine</a>  for capturing ideas and thoughts.
+            </p>
+            <div className="mt-8">
+              <a href="#explore">
+                <Button
+                  className="bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-3 px-8 rounded-full shadow-lg transition-all duration-300 ease-in-out"
+                >
+                  Explore
+                </Button>
+              </a>
+            </div>
+          </div>
+        </section>
+        <section id="explore" className="py-5">
+          <BlogCatalogue />
+        </section>
       </div>
     </section>
   );
--- src/components/blog-catalogue/index.tsx
+++ src/components/blog-catalogue/index.tsx
@@ -9,121 +9,120 @@ const BlogCatalogue = () => {
   const { pathname, fetchedBlogData } = useBlogCatalogue();
 
   return (
-    <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
-      {fetchedBlogData?.map((blog, index) => (
-        <div key={index} className="relative rounded-md border p-3">
-          {pathname.includes('/manage') && (
-            <>
-              <Button
-                href={`/blog/${blog.slug}`}
-                as={Link}
-                type="button"
-                isIconOnly
-                size="sm"
-                className="absolute right-12 top-2 bg-orange-500">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="#fff"
-                  className="size-4">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                  />
-                </svg>
-              </Button>
+    <div className='lg:max-w-[800px] mx-auto'>
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-1">
+        {fetchedBlogData?.map((blog, index) => (
+          <div key={index} className="relative rounded-lg border border-gray-300 p-4 shadow-sm hover:shadow-md transition-shadow duration-300">
+            {pathname.includes('/manage') && (
+              <>
+                <Button
+                  href={`/blog/${blog.slug}`}
+                  as={Link}
+                  type="button"
+                  isIconOnly
+                  size="sm"
+                  className="absolute right-16 top-3 bg-indigo-600 hover:bg-indigo-700 text-white rounded-full p-2 transition-colors duration-300">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-5 h-5">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                    />
+                  </svg>
+                </Button>
 
-              <Button
-                href={`/manage/update/${blog.slug}`}
-                as={Link}
-                type="button"
-                isIconOnly
-                size="sm"
-                className="absolute right-2 top-2 bg-blue-500">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="#fff"
-                  className="size-4">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-                  />
-                </svg>
-              </Button>
-            </>
-          )}
-          {pathname.includes('/manage') ? (
-            <div key={index}>
-              <h1 className="text-4xl">{blog.title}</h1>
-              <p className="my-5 line-clamp-3">{blog.summary}...</p>
+                <Button
+                  href={`/manage/update/${blog.slug}`}
+                  as={Link}
+                  type="button"
+                  isIconOnly
+                  size="sm"
+                  className="absolute right-3 top-3 bg-indigo-600 hover:bg-indigo-700 text-white rounded-full p-2 transition-colors duration-300">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-5 h-5">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                    />
+                  </svg>
+                </Button>
+              </>
+            )}
 
-              <div className='flex justify-between items-center flex-wrap'>
-                <div>
-                  {blog.tags.map((tag, index) => {
-                    return (
+            {pathname.includes('/manage') ?
+              <div>
+                <h1 className="text-2xl md:text-3xl font-semibold text-gray-800 dark:text-gray-100">
+                  {blog.title}
+                </h1>
+                <p className="my-4 text-gray-600 dark:text-gray-400 line-clamp-3">
+                  {blog.summary}...
+                </p>
+
+                <div className="flex justify-between items-center">
+                  <div className="flex flex-wrap">
+                    {blog.tags.map((tag, index) => (
                       <span
-                        className="rounded-full bg-gray-100 px-2 py-1"
                         key={index}
-                        style={{ marginRight: '5px' }}>
+                        className="text-sm bg-gray-200 text-indigo-600 rounded-full px-3 py-1 mr-2 mb-2">
                         {tag}
                       </span>
-                    );
-                  })}
-                </div>
-
-                <div className='text-slate-500'>
-
-                  {moment(new Date(blog.createdAt.seconds * 1000)).format(
-                    "D MMM YYYY, h:mm a"
-                  )}
-
+                    ))}
+                  </div>
+                  <div className="text-sm text-gray-500 dark:text-gray-400">
+                    {moment(new Date(blog.createdAt.seconds * 1000)).format('D MMM YYYY, h:mm a')}
+                  </div>
                 </div>
               </div>
-            </div>
-          ) : (
-            <Link href={`/blog/${blog.slug}`}>
-              <div key={index}>
-                <h1 className="text-4xl">{blog.title}</h1>
-                <p className="my-5 line-clamp-3">{blog.summary}...</p>
+              :
+              <Link href={`/blog/${blog.slug}`}>
+                <div className='group'>
+                  <h1 className="text-2xl group-hover:underline decoration-indigo-600 underline-offset-2 md:text-3xl font-bold text-gray-900 transition-all duration-300 dark:text-white">
+                    {blog.title}
+                  </h1>
+                  <p className="my-3 text-gray-700 dark:text-gray-300 line-clamp-3">
+                    {blog.summary}...
+                  </p>
 
-                <div className='flex justify-between items-center flex-wrap'>
-                  <div>
-                    {blog.tags.map((tag, index) => {
-                      return (
+                  <div className="flex justify-between items-center mt-4">
+                    <div className="flex flex-wrap">
+                      {blog.tags.map((tag, index) => (
                         <span
-                          className="rounded-full bg-gray-100 px-2 py-1"
                           key={index}
-                          style={{ marginRight: '5px' }}>
+                          className="text-xs md:text-sm bg-indigo-50 text-indigo-600 rounded-full px-2 py-1 mr-2 mb-2">
                           {tag}
                         </span>
-                      );
-                    })}
-                  </div>
-                  <div>
-                    <div className='text-slate-500'>
-                      {moment(new Date(blog.createdAt.seconds * 1000)).format(
-                        "D MMM YYYY, h:mm a"
-                      )}
+                      ))}
+                    </div>
+                    <div className="text-xs md:text-sm text-gray-500 dark:text-gray-400">
+                      {moment(new Date(blog.createdAt.seconds * 1000)).format('D MMM YYYY')}
                     </div>
                   </div>
                 </div>
-              </div>
-            </Link>
-          )}
-        </div>
-      ))}
+
+              </Link>
+            }
+
+          </div>
+
+        ))}
+      </div>
     </div>
   );
 };

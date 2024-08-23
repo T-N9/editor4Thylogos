389345ec0af6e0d1e60b7b6986afe65d112b815b
T-N9 <thethoriumtension@gmail.com>
2024-08-23 16:19:18 +0700

    [LogoText] updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <section className='text-black'>
       <div className='max-w-[1350px] mx-auto my-5 mb-10'>
-        <h1 className='text-2xl font-bold text-blue-500 mb-3'>TeNyain's articles</h1>
+        <h1 className='text-2xl font-bold text-blue-500 mb-3'>TN Notes</h1>
         <BlogCatalogue />
       </div>
     </section>

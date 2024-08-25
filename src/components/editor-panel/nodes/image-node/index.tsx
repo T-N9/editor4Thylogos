a518495d6da9bc142449f8c81d773132003f59ca
T-N9 <thethoriumtension@gmail.com>
2024-08-26 00:42:31 +0700

    [ImageWidth] testing

--- src/components/editor-panel/nodes/image-node/index.tsx
+++ src/components/editor-panel/nodes/image-node/index.tsx
@@ -8,273 +8,278 @@
  */
 
 import type {
-    DOMConversionMap,
-    DOMConversionOutput,
-    DOMExportOutput,
-    EditorConfig,
-    LexicalEditor,
-    LexicalNode,
-    NodeKey,
-    SerializedEditor,
-    SerializedLexicalNode,
-    Spread,
-  } from 'lexical';
-  
-  import {$applyNodeReplacement, createEditor, DecoratorNode} from 'lexical';
-  import * as React from 'react';
-  import {Suspense} from 'react';
+  DOMConversionMap,
+  DOMConversionOutput,
+  DOMExportOutput,
+  EditorConfig,
+  LexicalEditor,
+  LexicalNode,
+  NodeKey,
+  SerializedEditor,
+  SerializedLexicalNode,
+  Spread,
+} from 'lexical';
+
+import { $applyNodeReplacement, createEditor, DecoratorNode } from 'lexical';
+import * as React from 'react';
+import { Suspense } from 'react';
 import { contentSizers } from '../../content-resizer';
-  
-  const ImageComponent = React.lazy(() => import('./ImageComponent'));
-  
-  export interface ImagePayload {
+
+const ImageComponent = React.lazy(() => import('./ImageComponent'));
+
+export interface ImagePayload {
+  altText: string;
+  caption?: LexicalEditor;
+  height?: number;
+  key?: NodeKey;
+  maxWidth?: number;
+  showCaption?: boolean;
+  src: string;
+  width?: number;
+  captionsEnabled?: boolean;
+}
+
+function isGoogleDocCheckboxImg(img: HTMLImageElement): boolean {
+  return (
+    img.parentElement != null &&
+    img.parentElement.tagName === 'LI' &&
+    img.previousSibling === null &&
+    img.getAttribute('aria-roledescription') === 'checkbox'
+  );
+}
+
+function $convertImageElement(domNode: Node): null | DOMConversionOutput {
+  const img = domNode as HTMLImageElement;
+  if (img.src.startsWith('file:///') || isGoogleDocCheckboxImg(img)) {
+    return null;
+  }
+  const { alt: altText, src, width, height } = img;
+  const node = $createImageNode({ altText, height, src, width });
+  return { node };
+}
+
+export type SerializedImageNode = Spread<
+  {
     altText: string;
-    caption?: LexicalEditor;
+    caption: SerializedEditor;
     height?: number;
-    key?: NodeKey;
-    maxWidth?: number;
-    showCaption?: boolean;
+    maxWidth: number;
+    showCaption: boolean;
     src: string;
     width?: number;
-    captionsEnabled?: boolean;
+  },
+  SerializedLexicalNode
+>;
+
+export class ImageNode extends DecoratorNode<JSX.Element> {
+  __src: string;
+  __altText: string;
+  __width: 'inherit' | number;
+  __height: 'inherit' | number;
+  __maxWidth: number;
+  __showCaption: boolean;
+  __caption: LexicalEditor;
+  // Captions cannot yet be used within editor cells
+  __captionsEnabled: boolean;
+
+  static getType(): string {
+    return 'image';
   }
-  
-  function isGoogleDocCheckboxImg(img: HTMLImageElement): boolean {
-    return (
-      img.parentElement != null &&
-      img.parentElement.tagName === 'LI' &&
-      img.previousSibling === null &&
-      img.getAttribute('aria-roledescription') === 'checkbox'
+
+  static clone(node: ImageNode): ImageNode {
+    return new ImageNode(
+      node.__src,
+      node.__altText,
+      node.__maxWidth,
+      node.__width,
+      node.__height,
+      node.__showCaption,
+      node.__caption,
+      node.__captionsEnabled,
+      node.__key,
     );
   }
-  
-  function $convertImageElement(domNode: Node): null | DOMConversionOutput {
-    const img = domNode as HTMLImageElement;
-    if (img.src.startsWith('file:///') || isGoogleDocCheckboxImg(img)) {
-      return null;
+
+  static importJSON(serializedNode: SerializedImageNode): ImageNode {
+    const { altText, height, width, maxWidth, caption, src, showCaption } =
+      serializedNode;
+    const node = $createImageNode({
+      altText,
+      height,
+      maxWidth,
+      showCaption,
+      src,
+      width,
+    });
+    const nestedEditor = node.__caption;
+    const editorState = nestedEditor.parseEditorState(caption.editorState);
+
+    if (!editorState.isEmpty()) {
+      // console.log('Editor State is not empty', {editorState});
+      nestedEditor.setEditorState(editorState);
+    } else {
+      // console.log('Editor State is empty', {editorState});
     }
-    const {alt: altText, src, width, height} = img;
-    const node = $createImageNode({altText, height, src, width});
-    return {node};
+    return node;
   }
-  
-  export type SerializedImageNode = Spread<
-    {
-      altText: string;
-      caption: SerializedEditor;
-      height?: number;
-      maxWidth: number;
-      showCaption: boolean;
-      src: string;
-      width?: number;
-    },
-    SerializedLexicalNode
-  >;
-  
-  export class ImageNode extends DecoratorNode<JSX.Element> {
-    __src: string;
-    __altText: string;
-    __width: 'inherit' | number;
-    __height: 'inherit' | number;
-    __maxWidth: number;
-    __showCaption: boolean;
-    __caption: LexicalEditor;
-    // Captions cannot yet be used within editor cells
-    __captionsEnabled: boolean;
-  
-    static getType(): string {
-      return 'image';
-    }
-  
-    static clone(node: ImageNode): ImageNode {
-      return new ImageNode(
-        node.__src,
-        node.__altText,
-        node.__maxWidth,
-        node.__width,
-        node.__height,
-        node.__showCaption,
-        node.__caption,
-        node.__captionsEnabled,
-        node.__key,
-      );
-    }
-  
-    static importJSON(serializedNode: SerializedImageNode): ImageNode {
-      const {altText, height, width, maxWidth, caption, src, showCaption} =
-        serializedNode;
-      const node = $createImageNode({
-        altText,
-        height,
-        maxWidth,
-        showCaption,
-        src,
-        width,
+
+  exportDOM(): DOMExportOutput {
+    const element = document.createElement('img');
+    element.setAttribute('src', this.__src);
+    element.setAttribute('alt', this.__altText);
+    element.setAttribute('width', this.__width.toString());
+    element.setAttribute('height', this.__height.toString());
+    return { element };
+  }
+
+  static importDOM(): DOMConversionMap | null {
+    return {
+      img: (node: Node) => ({
+        conversion: $convertImageElement,
+        priority: 0,
+      }),
+    };
+  }
+
+  constructor(
+    src: string,
+    altText: string,
+    maxWidth: number,
+    width?: 'inherit' | number,
+    height?: 'inherit' | number,
+    showCaption?: boolean,
+    caption?: LexicalEditor,
+    captionsEnabled?: boolean,
+    key?: NodeKey,
+  ) {
+    super(key);
+    this.__src = src;
+    this.__altText = altText;
+    this.__maxWidth = maxWidth;
+    this.__width = width || 'inherit';
+    this.__height = height || 'inherit';
+    this.__showCaption = showCaption || false;
+    this.__caption =
+      caption ||
+      createEditor({
+        nodes: [],
       });
-      const nestedEditor = node.__caption;
-      const editorState = nestedEditor.parseEditorState(caption.editorState);
-
-      if (!editorState.isEmpty()) {
-        // console.log('Editor State is not empty', {editorState});
-        nestedEditor.setEditorState(editorState);
-      }else {
-        // console.log('Editor State is empty', {editorState});
-      }
-      return node;
-    }
-  
-    exportDOM(): DOMExportOutput {
-      const element = document.createElement('img');
-      element.setAttribute('src', this.__src);
-      element.setAttribute('alt', this.__altText);
-      element.setAttribute('width', this.__width.toString());
-      element.setAttribute('height', this.__height.toString());
-      return {element};
-    }
-  
-    static importDOM(): DOMConversionMap | null {
-      return {
-        img: (node: Node) => ({
-          conversion: $convertImageElement,
-          priority: 0,
-        }),
-      };
-    }
-  
-    constructor(
-      src: string,
-      altText: string,
-      maxWidth: number,
-      width?: 'inherit' | number,
-      height?: 'inherit' | number,
-      showCaption?: boolean,
-      caption?: LexicalEditor,
-      captionsEnabled?: boolean,
-      key?: NodeKey,
-    ) {
-      super(key);
-      this.__src = src;
-      this.__altText = altText;
-      this.__maxWidth = maxWidth;
-      this.__width = width || 'inherit';
-      this.__height = height || 'inherit';
-      this.__showCaption = showCaption || false;
-      this.__caption =
-        caption ||
-        createEditor({
-          nodes: [],
-        });
-      this.__captionsEnabled = captionsEnabled || captionsEnabled === undefined;
-    }
-  
-
-    exportJSON(): SerializedImageNode {
-      return {
-        altText: this.getAltText(),
-        caption: this.__caption.toJSON(),
-        height: this.__height === 'inherit' ? 0 : this.__height,
-        maxWidth: this.__maxWidth,
-        showCaption: this.__showCaption,
-        src: this.getSrc(),
-        type: 'image',
-        version: 1,
-        width: this.__width === 'inherit' ? 0 : this.__width,
-      };
-    }
-  
-    setWidthAndHeight(
-      width: 'inherit' | number,
-      height: 'inherit' | number,
-    ): void {
-      const writable = this.getWritable();
-      writable.__width = width;
-      writable.__height = height;
-    }
-  
-    setShowCaption(showCaption: boolean): void {
-      const writable = this.getWritable();
-      writable.__showCaption = showCaption;
-    }
-  
-    // View
-  
-    createDOM(config: EditorConfig): HTMLElement {
-      const span = document.createElement('span');
-      const theme = config.theme;
-      const className = theme.image;
-      if (className !== undefined) {
-        span.className = className;
-      }
-      return span;
-    }
-  
-    updateDOM(): false {
-      return false;
-    }
-  
-    getSrc(): string {
-      return this.__src;
-    }
-  
-    getAltText(): string {
-      return this.__altText;
-    }
-  
-    decorate(): JSX.Element {
-      return (
-        <Suspense fallback={null}>
-          <ImageComponent
-            src={this.__src}
-            altText={this.__altText}
-            width={this.__width}
-            height={this.__height}
-            maxWidth={this.__maxWidth}
-            nodeKey={this.getKey()}
-            showCaption={this.__showCaption}
-            caption={this.__caption}
-            captionsEnabled={this.__captionsEnabled}
-            resizable={true}
-          />
-        </Suspense>
-      );
-    }
+    this.__captionsEnabled = captionsEnabled || captionsEnabled === undefined;
   }
-  
-  export function $createImageNode({
-    altText,
-    height,
-    maxWidth,
-    captionsEnabled,
-    src,
-    width,
-    showCaption,
-    caption,
-    key,
-  }: ImagePayload): ImageNode {
-
-    const localStorageKey = localStorage.getItem('my-editor-state-key');
-    const localState = localStorageKey && JSON.parse(localStorageKey) || false;
 
 
-    // console.log({state : parseInt(contentSizers[localState.contentSize].size)});
-    return $applyNodeReplacement(
-      new ImageNode(
-        src,
-        altText,
-        maxWidth = localState ? parseInt(contentSizers[localState.contentSize].size)-60 : 1350,
-        width,
-        height,
-        showCaption,
-        caption,
-        captionsEnabled,
-        key,
-      ),
+  exportJSON(): SerializedImageNode {
+    return {
+      altText: this.getAltText(),
+      caption: this.__caption.toJSON(),
+      height: this.__height === 'inherit' ? 0 : this.__height,
+      maxWidth: this.__maxWidth,
+      showCaption: this.__showCaption,
+      src: this.getSrc(),
+      type: 'image',
+      version: 1,
+      width: this.__width === 'inherit' ? 0 : this.__width,
+    };
+  }
+
+  setWidthAndHeight(
+    width: 'inherit' | number,
+    height: 'inherit' | number,
+  ): void {
+    const writable = this.getWritable();
+    writable.__width = width;
+    writable.__height = height;
+  }
+
+  setShowCaption(showCaption: boolean): void {
+    const writable = this.getWritable();
+    writable.__showCaption = showCaption;
+  }
+
+  // View
+
+  createDOM(config: EditorConfig): HTMLElement {
+    const span = document.createElement('span');
+    const theme = config.theme;
+    const className = theme.image;
+    if (className !== undefined) {
+      span.className = className;
+    }
+    return span;
+  }
+
+  updateDOM(): false {
+    return false;
+  }
+
+  getSrc(): string {
+    return this.__src;
+  }
+
+  getAltText(): string {
+    return this.__altText;
+  }
+
+  decorate(): JSX.Element {
+    return (
+      <Suspense fallback={null}>
+        <ImageComponent
+          src={this.__src}
+          altText={this.__altText}
+          width={this.__width}
+          height={this.__height}
+          maxWidth={this.__maxWidth}
+          nodeKey={this.getKey()}
+          showCaption={this.__showCaption}
+          caption={this.__caption}
+          captionsEnabled={this.__captionsEnabled}
+          resizable={true}
+        />
+      </Suspense>
     );
   }
-  
-  export function $isImageNode(
-    node: LexicalNode | null | undefined,
-  ): node is ImageNode {
-    return node instanceof ImageNode;
+}
+
+export function $createImageNode({
+  altText,
+  height,
+  maxWidth,
+  captionsEnabled,
+  src,
+  width,
+  showCaption,
+  caption,
+  key,
+}: ImagePayload): ImageNode {
+
+  // Initialize localState to false by default
+  let localState = null;
+
+  // Check if window is defined to ensure we're in the browser environment
+  if (typeof window !== 'undefined') {
+    const localStorageKey = localStorage.getItem('my-editor-state-key');
+    localState = localStorageKey ? JSON.parse(localStorageKey) : false;
   }
-  
+
+
+  // console.log({state : parseInt(contentSizers[localState.contentSize].size)});
+  return $applyNodeReplacement(
+    new ImageNode(
+      src,
+      altText,
+      maxWidth = localState ? parseInt(contentSizers[localState.contentSize].size) - 60 : 1350,
+      width,
+      height,
+      showCaption,
+      caption,
+      captionsEnabled,
+      key,
+    ),
+  );
+}
+
+export function $isImageNode(
+  node: LexicalNode | null | undefined,
+): node is ImageNode {
+  return node instanceof ImageNode;
+}

d09ceab42ae720db246a8563872883e021c846c4
T-N9 <thethoriumtension@gmail.com>
2024-08-26 13:27:15 +0700

    [meta] image testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import { NextUIProvider } from "@nextui-org/react";
 import Footer from "@/components/footer/Footer";
 
 export const metadata: Metadata = {
-  title: "TN Notes",
+  title: "TN Notes | Capturing ideas and thoughts.",
   description: "Welcome to TN Notes! A tiny space of mine for capturing ideas and thoughts.",
 };
 
@@ -20,11 +20,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:image" content="/meta.png" />
+      </head>
       <body className="text-black">
         <NextUIProvider>
           <EditorStateProvider>
             {children}
-            <Footer/>
+            <Footer />
           </EditorStateProvider>
         </NextUIProvider>
       </body>

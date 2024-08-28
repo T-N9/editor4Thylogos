11c5caa094280e3faf8f61eaa567e85b7c19d535
T-N9 <thethoriumtension@gmail.com>
2024-08-28 13:06:03 +0700

    [Sepia & B&W] theme testing

--- src/components/ui/ThemeSwitcher.tsx
+++ src/components/ui/ThemeSwitcher.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
-import { Button, Switch } from '@nextui-org/react';
+import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Switch } from '@nextui-org/react';
 import Link from 'next/link';
 import Image from 'next/image';
 
@@ -24,12 +24,36 @@ const ThemeSwitcher = () => {
                 {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                 </svg> */}
-                <Image src={'/images/Logo.png'} width={50} height={50} alt="TN Notes Logo"/>
+                <Image src={'/images/Logo.png'} width={50} height={50} alt="TN Notes Logo" />
             </Button>
 
+            <Dropdown className='dark:text-white'>
+                <DropdownTrigger>
+                    <Button
+                        variant="bordered"
+                        className="capitalize"
+                    >
+                        {theme}
+                    </Button>
+                </DropdownTrigger>
+                <DropdownMenu
+                    aria-label="Single selection example"
+                    variant="flat"
+                    disallowEmptySelection
+                    selectionMode="single"
+                    selectedKeys={theme}
+                // onSelectionChange={setTheme}
+                >
+                    <DropdownItem onClick={() => setTheme('light')} key="text">Light</DropdownItem>
+                    <DropdownItem onClick={() => setTheme('dark')} key="number">Night</DropdownItem>
+                    <DropdownItem onClick={() => setTheme('sepia')} key="date">Sepia</DropdownItem>
+                    <DropdownItem onClick={() => setTheme('grayscale')} key="B&B">B&W</DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
 
 
-            <Switch className='relative' onClick={() => {
+
+            {/* <Switch className='relative' onClick={() => {
 
                 if (theme === 'light') {
                     setTheme('dark')
@@ -49,7 +73,7 @@ const ThemeSwitcher = () => {
                         </>
                     }
                 </span>
-            </Switch>
+            </Switch> */}
         </div>
     );
 };

b736f4c09c3528614a9201f7b1717553bf2b5e5e
T-N9 <thethoriumtension@gmail.com>
2024-10-23 17:34:45 +0700

    💄 Blog Titles updated

--- src/components/blog-post/index.tsx
+++ src/components/blog-post/index.tsx
@@ -192,13 +192,9 @@ const BlogPost = ({ title, slug, image, imageCaption, tags, createdAt, editorSta
                                 </div>
 
                                 <div className="flex gap-2 items-center  dark:text-gray-400">
-                                    <a href='/'
-                                        onClick={(e) => {
-                                            e.preventDefault()
-                                            router.back()
-                                        }}>
+                                    <Link href='/'>
                                         <span className="text-sm ">Blog</span>
-                                    </a>
+                                    </Link>
 
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
